515a1b16abd2f1478dd40128593e9ef2a4d1b577
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-04-15 10:08:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Descargador de YouTube a MP3
+Este es un simple descargador de videos de YouTube que convierte los videos descargados a archivos de audio MP3. La aplicación está construida con Python utilizando las bibliotecas Pytube y Pydub, y se ejecuta en Streamlit para crear una interfaz de usuario web amigable.
+
+## Instalación
+Clona este repositorio en tu máquina local:
+
+``
+git clone https://github.com/tu_usuario/descargador-youtube-mp3.git
+``
+
+Navega al directorio del proyecto:
+
+``
+cd descargador-youtube-mp3
+``
+
+Instala las dependencias utilizando pip:
+
+`` 
+pip install -r requirements.txt
+``
+
+Ejecuta la aplicación utilizando Streamlit:
+
+
+``
+streamlit run app.py
+``
+
+Abre tu navegador web y ve a la dirección proporcionada por Streamlit.
+
+Introduce la URL del video de YouTube en el campo de entrada.
+
+Haz clic en el botón "Descargar y Convertir" para descargar el video y convertirlo a MP3.
+
+Una vez que se complete la conversión, podrás reproducir el audio directamente en la aplicación o descargarlo haciendo clic en el enlace proporcionado.
+
+## Contribuciones
+¡Las contribuciones son bienvenidas! Si encuentras algún error o tienes alguna mejora que sugerir, no dudes en abrir un issue o enviar un pull request.
+
+## Licencia
+Este proyecto está licenciado bajo la Licencia MIT.
---
+++ script.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from pytube import YouTube
+from pydub import AudioSegment
+import os
+
+# Función para descargar el video de YouTube
+def descargar_video(url, ruta):
+    yt = YouTube(url)
+    # Seleccionar la mejor calidad disponible
+    video = yt.streams.filter(only_audio=True).first()
+    # Descargar el video
+    video.download(ruta)
+    return video.default_filename
+
+# Función para convertir el video descargado a mp3
+def convertir_a_mp3(video_file, ruta):
+    # Construir la ruta completa al archivo de video
+    video_file_path = os.path.join(ruta, video_file)
+    # Cargar el archivo de audio usando pydub
+    audio = AudioSegment.from_file(video_file_path)
+    # Crear el nombre del archivo mp3
+    mp3_file = os.path.splitext(video_file)[0] + '.mp3'
+    # Guardar el archivo de audio como mp3 en la carpeta music
+    mp3_file_path = os.path.join(ruta, 'music', mp3_file)
+    audio.export(mp3_file_path, format='mp3')
+    # Eliminar el archivo de video
+    os.remove(video_file_path)
+    return mp3_file
+
+# Función principal de la aplicación
+def main():
+    st.title("De video a MP3")
+    st.write("Esta aplicación te permite descargar y convertir videos de YouTube a archivos de audio MP3.")
+    st.write("Para poder utilizar esta aplicación, simplemente introduce la URL del video de YouTube en el cuadro de texto y haz clic en el botón 'Descargar y Convertir")
+
+    # Entrada de URL de YouTube
+    url = st.text_input("Introduce la URL del video de YouTube:")
+    # OsCreate folder called music
+    if not os.path.exists('music'):
+        os.makedirs('music')
+
+    if st.button("Descargar y Convertir"):
+        # Validar la URL
+        if url.strip() == "":
+            st.error("Por favor, introduce una URL válida.")
+        else:
+            try:
+                # Ruta donde se guardarán los archivos
+                ruta = os.path.abspath(os.path.dirname(__file__))
+                # Descargar el video, meterlo en la carpeta music y obtener su nombre de archivo
+                video_file = descargar_video(url, ruta)
+                # Convertir el video a MP3
+                mp3_file = convertir_a_mp3(video_file, ruta)
+                st.success("El video se ha descargado y convertido a MP3 con éxito.")
+                st.audio(mp3_file, format='audio/mp3')
+                st.markdown(f"[Descargar archivo MP3]({mp3_file})")
+            except Exception as e:
+                st.error(f"Ha ocurrido un error: {e}")
+    
+    st.write("Hecho por Enrique Rodriguez Vela")
+
+if __name__ == "__main__":
+    main()
